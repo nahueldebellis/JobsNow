07ba28d71f5f97180874bf39db400c537e064831
Practicantes M <practicantesm@unlam.edu.ar>
2019-06-18 11:56:59 -0300

    fix image profile, missing additional data, pdf problems

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -1,5 +1,5 @@
 <?php
-    use Phalcon\Mvc\Controller;
+    use Phalcon\Mvc\Controller, Model;
 
     class ProfileController extends Controller{
         public function indexAction(){
@@ -8,25 +8,54 @@
                 "id = $user_id" 
             ]);
 
-            //$this->view->disable();
+            //if($user_info->type == "Employee"){
+            $aditional_info = Employee::findFirst([
+                "user_id = $user_id",
+            ]);
+            /*}
+            elseif($user_info->type == "Company"){
+                $aditional_info = Company::findFirst([
+                    "user_id = $user_id",
+                ]);
+            }*/
             $this->view->info = [
+                'id' => $user_info->id,
                 'name' => $user_info->name,
                 'email'=> $user_info->email,
                 'phone'=> $user_info->phone,
                 'address'=> $user_info->address,
                 'rotulo'=> $user_info->rotulo,
-                'profile_photo'=> $user_info->profile_photo
+                'profile_photo'=> $user_info->profile_photo,
             ];
+
+            foreach($aditional_info as $value){
+                $this->view->info["$campo"] = $value;
+            }
+
+            //$this->view->disable();
+
         }
 
-        public function imageAction(){
+        public function cvAction(){
             $user_id = $this->request->getQuery('user_id');
-            $user_info = Users::findFirst([
-                "id = $user_id" 
+            $employee_info = Employee::findFirst([
+                "user_id = $user_id",
             ]);
 
-            $this->response->setHeader("Content-Type", "image/jpeg");
-            $this->response->setContent(base64_decode( $user_info->profile_photo ));
+           
+            $data = base64_decode($employee_info->cv);
+
+            $this->response->setHeader('Content-Type', 'application/pdf');
+            $this->response->setHeader('Content-Disposition', 'attachment; filename="filename.pdf"');
+            $this->response->setHeader('Content-Transfer-Encoding', 'binary');
+            $this->response->setHeader('Accept-Ranges', 'bytes');
+            $this->response->setContent($data);
+            $this->response->send();
+            
+            /*$this->response->setHeader("Content-Type", "application/pdf");
+            $this->response->setHeader("Content-Disposition", 'attachment; filename="filename.pdf"');
+            $file = $employee_info->cv;
+            $this->response->setFileToSend($file);*/
         }
 
     }
