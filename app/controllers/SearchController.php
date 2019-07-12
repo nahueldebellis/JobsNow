e7e902ade8387523bc6d51731d78171fba637149
Practicantes M <practicantesm@unlam.edu.ar>
2019-07-12 11:57:30 -0300

    add search controller and view

---
+++ app/controllers/SearchController.php
@@ -0,0 +1,41 @@
+<?php
+use Phalcon\Mvc\Controller;
+
+class SearchController extends Controller{
+    public function indexAction(){
+        $search_query = $this->request->getJsonRawBody();
+        $search_query = $search_query->search;
+        
+        $out = $this->getSearchData($search_query);        
+            
+        $this->view->disable();
+        $this->response->setContentType('application/json', 'UTF-8');
+        echo json_encode($out);
+    }    
+
+    public function resultsAction(){
+        $this->assets->addCss("css/main_template.css");
+        $this->assets->addCss("css/results.css");
+        $this->view->setTemplateBefore('main_template');
+        $search_query = $this->request->getPost('search');
+        $out = $this->getSearchData($search_query);        
+        $this->view->search_results = ($out);
+    }
+
+    private function getSearchData($search_query){
+        $search_result = Users::find([
+            "columns" => 'id, name, rotulo, email, phone, address',
+            "conditions" => "rotulo LIKE '%$search_query%' OR name LIKE '%$search_query%' OR email LIKE '%$search_query%'"
+        ]);
+        $out = [];
+        foreach($search_result as $user){
+            array_push($out, $user);
+        }
+        
+        return $out;
+    }
+}
+
+
+
+?>
